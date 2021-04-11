ca75f77a372721d77d08711dbaa690e470548a19
Ciro Goyeneche <soycirog@gmail.com>
2021-04-11 16:51:00 -0300

    Render index.html template

--- pyspace/main/routes.py
+++ pyspace/main/routes.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from .utils import get_space_launches
 
-main = Blueprint('main', __name__)
+main = Blueprint('main', __name__, template_folder="templates")
 
 
 @main.route('/')
 def home():
     launches = get_space_launches()
     print(launches)
-    return '<h1>This is the home page</h1>'
+    return render_template('index.html')
