036b49200273f516b946a3d1c21b6e993d0cd188
Ciro Goyeneche <soycirog@gmail.com>
2021-04-11 16:48:18 -0300

    Imported get_space_launches function and used it in home route

--- pyspace/main/routes.py
+++ pyspace/main/routes.py
@@ -1,8 +1,11 @@
 from flask import Blueprint
+from .utils import get_space_launches
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def home():
+    launches = get_space_launches()
+    print(launches)
     return '<h1>This is the home page</h1>'
