b18f7d42bf47116320da849ab88798b841ca66b8
Ciro Goyeneche <soycirog@gmail.com>
2021-04-13 20:57:32 -0300

    Added default parameter to get_space_launches function

--- pyspace/main/utils.py
+++ pyspace/main/utils.py
@@ -6,8 +6,8 @@ import requests
 import json
 
 
-def get_space_launches():
-    API_END_POINT = "https://ll.thespacedevs.com/2.0.0/launch/?limit=10"
+def get_space_launches(limit=10):
+    API_END_POINT = f"https://ll.thespacedevs.com/2.0.0/launch/?limit={limit}"
     launches = requests.get(API_END_POINT)
     launches_data = json.loads(launches.text)
 
