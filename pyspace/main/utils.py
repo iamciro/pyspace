0aad43af671933e5b0f4ac8a86424047da26ea0f
Ciro Goyeneche <soycirog@gmail.com>
2021-04-13 21:35:06 -0300

    Added exception handling if the API fails.

--- pyspace/main/utils.py
+++ pyspace/main/utils.py
@@ -7,8 +7,14 @@ import json
 
 
 def get_space_launches(limit=10):
-    API_END_POINT = f"https://ll.thespacedevs.com/2.0.0/launch/?limit={limit}"
-    launches = requests.get(API_END_POINT)
-    launches_data = json.loads(launches.text)
-
-    return launches_data
+    '''
+        This function calls to the Space Launch API and gets Space Launches.
+        It returns a list of dictionaries if they're found and None otherwise.
+    '''
+    try:
+        API_END_POINT = f"https://ll.thespacedevs.com/2.0.0/launch/?mode=list&limit={limit}"
+        launches = requests.get(API_END_POINT)
+        launches_data = json.loads(launches.text)
+        return launches_data['results']
+    except KeyError:
+        return None
