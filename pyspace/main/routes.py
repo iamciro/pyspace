52b3a366ae645722c595c5e51307dbd6ad3cf828
Ciro Goyeneche <soycirog@gmail.com>
2021-04-14 22:14:55 -0300

    convert_str_to_datetime function imported and passed to home route

--- pyspace/main/routes.py
+++ pyspace/main/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from .utils import get_space_launches
+from .utils import get_space_launches, convert_str_to_datetime
 
 main = Blueprint('main', __name__, template_folder="templates")
 
@@ -8,5 +8,7 @@ main = Blueprint('main', __name__, template_folder="templates")
 def home():
     launches = get_space_launches(10)
     if launches:
-        return render_template('index.html', launches=launches)
+        return render_template('index.html',
+                               launches=launches,
+                               convert_str_to_datetime=convert_str_to_datetime)
     return render_template('index.html')
